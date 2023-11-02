0ef6aa36cb1370b8bdd5ed8097285a5b596c2635
Tim <timcarlsson0203@gmail.com>
2023-11-02 19:03:49 +0100

    initial boiler plate

---
+++ password-manager/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.passwordmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.passwordmanager to javafx.fxml;
+    exports com.example.passwordmanager;
+}

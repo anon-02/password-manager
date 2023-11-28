727945d7096a13adfff51d27f298acb508759fe0
Tim <timcarlsson0203@gmail.com>
2023-11-28 18:55:55 +0100

    initial commit

--- src/main/java/com/example/passwordmanager/Model/dbStuff/DatabaseHandler.java
+++ src/main/java/com/example/passwordmanager/Model/dbStuff/DatabaseHandler.java
@@ -3,7 +3,7 @@ package com.example.passwordmanager.Model.dbStuff;
 import java.sql.*;
 
 public class DatabaseHandler {
-    private static final String JDBC_URL = "jdbc:sqlite:C:/Users/Felix/IdeaProjects/password-manager/Database/users.db";
+    private static final String JDBC_URL = "jdbc:sqlite:C:/Users/Tim/Chalmers/test/password-manager/Database/users.db";
     private static Connection  connection;
 
     private DatabaseHandler() {
